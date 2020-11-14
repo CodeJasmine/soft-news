33037c85b161630e7f1b2728284359aa96d08161
CodeJasmine <2816540069@qq.com>
2020-11-15 00:07:40 +0800

    Swagger2接口文档工具的使用

--- soft-news-service-api/src/main/java/com/soft1851/api/controller/user/UserControllerApi.java
+++ soft-news-service-api/src/main/java/com/soft1851/api/controller/user/UserControllerApi.java
@@ -1,16 +1,42 @@
 package com.soft1851.api.controller.user;
 
+import com.soft1851.result.GraceResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author wususu
  * @date 2020/11/14 22:20
  */
+@Api(value = "用户信息相关Controller", tags = {"用户信息相关Controller"})
+@RequestMapping("user")
 public interface UserControllerApi {
     /**
      * 获取所有用户
-     * @return users：所有用户信息
+     * @return
      */
     @GetMapping("/users")
     Object getUsers();
+
+    /**
+     * 获得所有用户信息
+     * @return 获得所有用户信息
+     */
+    @ApiOperation(value = "获得所有用户信息", notes = "获得所有用户信息", httpMethod = "POST")
+    @PostMapping("/all")
+    GraceResult getAllUsers();
+
+    /**
+     * 获得用户基本信息
+     * @param userId :用户id
+     * @return 用户基本信息
+     */
+    @ApiOperation(value = "获得用户基本信息", notes = "获得用户基本信息", httpMethod = "POST")
+    @PostMapping("/userInfo")
+    GraceResult getUserInfo(@RequestParam String userId);
+
 }
