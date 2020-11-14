85ef0b5d4302b8fe40c031a18d3912d57b4f697b
CodeJasmine <2816540069@qq.com>
2020-11-14 21:15:02 +0800

    配置logback日志与多环境profile

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.HelloControllerApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/11/14 20:26
  */
 @RestController
+@Slf4j
 public class HelloController implements HelloControllerApi {
     @Override
     public Object hello(){
+        log.info("info: hello");
+        log.warn("warn: hello");
+        log.error("error: hello");
         return "hello";
     }
 }
