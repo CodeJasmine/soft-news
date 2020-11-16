cdaa174a06c2527f1810671fc676f150ce256749
CodeJasmine <2816540069@qq.com>
2020-11-16 21:58:42 +0800

    注册登录功能

--- soft-news-service-api/src/main/java/com/soft1851/api/controller/user/PassportControllerApi.java
+++ soft-news-service-api/src/main/java/com/soft1851/api/controller/user/PassportControllerApi.java
@@ -1,28 +1,47 @@
 package com.soft1851.api.controller.user;
 
+import com.soft1851.pojo.bo.RegistLoginBO;
 import com.soft1851.result.GraceResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 /**
  * @author wususu
  * @date 2020/11/15 22:24
  */
-@Api(value = "用户注册登录",tags = {"用户注册登录的controller"})
+@Api(value = "用户注册登录", tags = {"用户注册登录的controller"})
 @RequestMapping("/passport")
 public interface PassportControllerApi {
     /**
      * 发送短信
-     * @param mobile 手机号
+     *
+     * @param mobile  手机号
      * @param request 请求
      * @return 统一响应
      */
     @ApiOperation(value = "获得短信验证码", notes = "获得短信验证码", httpMethod = "GET")
     @GetMapping("/smsCode")
     GraceResult getCode(@RequestParam String mobile, HttpServletRequest request);
+
+
+    /**
+     *
+     * @param registLoginBO
+     * @param result
+     * @param request
+     * @param response
+     * @return
+     */
+    @ApiOperation(value = "一键注册登录接口", notes = "一键注册登录接口", httpMethod = "POST")
+    @PostMapping("/sign")
+    GraceResult doSign(@RequestBody @Valid RegistLoginBO registLoginBO,
+                       BindingResult result,
+                       HttpServletRequest request,
+                       HttpServletResponse response);
 }
